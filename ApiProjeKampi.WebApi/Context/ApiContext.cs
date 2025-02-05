11881525890e22330d181e4981d7e3198e0e475b
MustafaSonmezisik <mstf.snmzisik@gmail.com>
2025-02-05 15:13:07 +0300

    5. Ders migration

---
+++ ApiProjeKampi.WebApi/Context/ApiContext.cs
@@ -0,0 +1,27 @@
+﻿using ApiProjeKampi.WebApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiProjeKampi.WebApi.Context
+{
+    public class ApiContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-9DKRCP6\\SQLEXPRESS;Initial catalog=ApiYummyDb;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Chef> Chefs { get; set; }
+        public DbSet<Contact>  Contacts { get; set; }
+        public DbSet<Feature>  Features { get; set; }
+        public DbSet<Image>  Images { get; set; }
+        public DbSet<Message>   Messages { get; set; }
+        public DbSet<Product>   Products  { get; set; }
+        public DbSet<Reservation>   Reservations  { get; set; }
+        public DbSet<Service>   Services  { get; set; }
+        public DbSet<Testimonial>  Testimonials { get; set; }
+
+
+
+
+    }
+}
