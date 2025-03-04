9cabc546d95cb34428f11211c0b84e2494cc49df
MustafaSonmezisik <mstf.snmzisik@gmail.com>
2025-03-04 16:59:58 +0300

    #7 Api Proje Kampı - Kategori Api İşlemlerinin Tamamlanması

--- ApiProjeKampi.WebApi/Controllers/CategoriesController.cs
+++ ApiProjeKampi.WebApi/Controllers/CategoriesController.cs
@@ -15,6 +15,12 @@ namespace ApiProjeKampi.WebApi.Controllers
         {
             _context = context;
         }
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+            var values = _context.Categories.ToList();
+            return Ok(values);
+        }
 
         [HttpPost]
         public IActionResult CreateCategory(Category category)
@@ -24,6 +30,31 @@ namespace ApiProjeKampi.WebApi.Controllers
             _context.SaveChanges();
             return Ok("Kategori Eklendi");
         }
-       
+
+
+        [HttpDelete]
+        public IActionResult DeleteCategory(int id) {
+            var value = _context.Categories.Find(id);
+            _context.Categories.Remove(value);
+            _context.SaveChanges();
+            return Ok("Kategori Silindi");
+        }
+
+        [HttpGet("GetCategory")]
+        public IActionResult GetCategory(int id)
+        {
+            var value = _context.Categories.Find(id);
+            return Ok(value);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCategory(Category category)
+        {
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+            return Ok("Kategori Güncellendi");
+        }
+
+
     }
 }
