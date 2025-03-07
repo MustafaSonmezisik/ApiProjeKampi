5aeb695431ffbbfdd2d58d2abc5b39ff12b38228
MustafaSonmezisik <mstf.snmzisik@gmail.com>
2025-03-07 16:40:12 +0300

    #14  Fluent Validation Kullanımı

---
+++ ApiProjeKampi.WebApi/ValidationRules/ProductValidator.cs
@@ -0,0 +1,21 @@
+﻿using ApiProjeKampi.WebApi.Entities;
+using FluentValidation;
+
+namespace ApiProjeKampi.WebApi.ValidationRules
+{
+    public class ProductValidator :AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(p => p.ProductName).NotEmpty().WithMessage("Ürün adı boş olamaz");
+            RuleFor(x => x.ProductName).MinimumLength(2).WithMessage("Ürün adı en az 2 karakter olmalıdır");
+            RuleFor(x => x.ProductName).MaximumLength(50).WithMessage("Ürün adı en fazla 50 karakter olmalıdır");
+
+            RuleFor(p => p.Price).NotEmpty().WithMessage("Ürün fiyatı boş olamaz").LessThan(0).WithMessage("Ürün fiyatı negatif olamaz")
+                                 .GreaterThan(1000).WithMessage("Ürün fiyatı bu kadar yüksek olamaz");
+
+
+            RuleFor(RuleFor => RuleFor.ProductDescription).NotEmpty().WithMessage("Ürün açıklaması boş olamaz");
+        }
+    }
+}
