ed05c43de8501159c265ecc1eb65cca8ff7fbf8d
MustafaSonmezisik <mstf.snmzisik@gmail.com>
2025-03-07 16:53:22 +0300

    Validation

--- ApiProjeKampi.WebApi/ValidationRules/ProductValidator.cs
+++ ApiProjeKampi.WebApi/ValidationRules/ProductValidator.cs
@@ -10,12 +10,11 @@ namespace ApiProjeKampi.WebApi.ValidationRules
             RuleFor(p => p.ProductName).NotEmpty().WithMessage("Ürün adı boş olamaz");
             RuleFor(x => x.ProductName).MinimumLength(2).WithMessage("Ürün adı en az 2 karakter olmalıdır");
             RuleFor(x => x.ProductName).MaximumLength(50).WithMessage("Ürün adı en fazla 50 karakter olmalıdır");
-
             RuleFor(p => p.Price).NotEmpty().WithMessage("Ürün fiyatı boş olamaz").LessThan(0).WithMessage("Ürün fiyatı negatif olamaz")
                                  .GreaterThan(1000).WithMessage("Ürün fiyatı bu kadar yüksek olamaz");
-
-
             RuleFor(RuleFor => RuleFor.ProductDescription).NotEmpty().WithMessage("Ürün açıklaması boş olamaz");
+
+
         }
     }
 }
