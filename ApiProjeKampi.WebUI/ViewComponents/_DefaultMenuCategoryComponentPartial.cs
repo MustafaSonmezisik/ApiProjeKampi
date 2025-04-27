4bb75ddbf88f9074dc84350d11d01099ce6d9be0
MustafaSonmezisik <mstf.snmzisik@gmail.com>
2025-04-27 22:06:14 +0300

    Kategorilerin UI Kısmında Consume İşlemi

---
+++ ApiProjeKampi.WebUI/ViewComponents/_DefaultMenuCategoryComponentPartial.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using ApiProjeKampi.WebUI.Dtos.CategoryDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiProjeKampi.WebUI.ViewComponents
+{
+    public class _DefaultMenuCategoryComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultMenuCategoryComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = client
+                .GetAsync("https://localhost:7105/api/Categories")
+                .GetAwaiter()
+                .GetResult();
+            ;
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = responseMessage.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+            }
+
+            return View();
+        }
+    }
+}
